cc4db06a003cec645ff536ca610cffe1375a6f13
Edilson Gonçalves Oliveira <101567222+EdilsonXX@users.noreply.github.com>
2022-06-08 12:14:24 -0300

    Update Lista.java

--- ListaEncadeada/src/br/ifba/edu/lista/Lista.java
+++ ListaEncadeada/src/br/ifba/edu/lista/Lista.java
@@ -1,12 +1,13 @@
 package br.ifba.edu.lista;
 
+import javax.swing.JOptionPane;
+
 import br.ifba.edu.basico.Celula;
 
 public class Lista {
 
 	private Celula cabeca;
 	private Celula cauda;
-
 	private int totalElementos = 0;
 	
 	public void adicionarNoComeco (Object obj) {
@@ -66,6 +67,45 @@ public class Lista {
 			this.totalElementos++;
 
 		}
+	}
+	
+	
+	public boolean verificarPosicao (int posicao) {
+		
+		return posicao >= 0 && posicao < this.totalElementos;
+		
+	}
+	
+	public void adicionarPorPosicao (Object obj, int posicao) {
+		
+		Celula aux = this.cabeca;
+		
+	if (posicao == 0) {
+			
+			this.adicionarNoComeco(obj);
+			
+		} else if (posicao == this.totalElementos) {
+			
+			this.adicionarNoFinal(obj);
+			
+		} else {
+			
+			if (!this.verificarPosicao(posicao)) {
+			
+				JOptionPane.showMessageDialog(null, "Posicao inválida", null, JOptionPane.ERROR_MESSAGE);
+				throw new IllegalArgumentException("A posição digitada é invalida");
+			}
+		}
+			
+			for (int i = 0; i < posicao - 1; i++) {
+			
+				aux = aux.getProxima();
+				
+		}
+			Celula nova = new Celula(obj);
+			nova.setProxima(aux.getProxima());
+			aux.setProxima(nova);
+			
 	}
 	
 }
